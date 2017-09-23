4415b8bd467f993beec1b93ea5de85f3006f2442
sombreslames <proferrari.leon@gmail.com>
2017-09-23 20:03:46 +0200

    Splitting in function

--- main.jl
+++ main.jl
@@ -1,70 +1,123 @@
-using JuMP
-using GLPKMathProgInterface
-
+#Ferrari Leon
+#M1 ORO
+#Version de test
+#Julia JuMP
+#DM1 - Metaheuristiques
+module DM1_metaheuristics
+using JuMP, GLPKMathProgInterface, PyPlot, myHeuristics.jl
 type Problem
-         Variables
-         LeftMembers_Constraints
-         RightMembers_Constraints
+   NBvariables::Int
+   NBconstraints::Int
+   Variables::Vector(NBvariables)
+   LeftMembers_Constraints::spzeros(NBconstraints,NBvariables)
+   RightMembers_Constraints::Vector(NBconstraints)
 end
 
-#MODEL CONSTRUCTION
-#--------------------
-#m           = Model(solver=GLPKSolverLP())
-m           = Model(solver=GLPKSolverMIP())
-#READING DATA FROM FILE
-#     GETTING NB OF VARIABLES AND CONSTRAINTS
-#     AND VALUES ASSOCIATED TO THEM
-files       = open("/home/jack/ORO/MetaHeuristics/Projet/DM1/Data/pb_100rnd0100.dat");
-lines       = readlines(files)
-counter     = 1
-counter2    = 1
-VarAndCons  = split(lines[1])
-NBcons      = parse(VarAndCons[1])
-NBvar       = parse(VarAndCons[2])
-
-values      = split(lines[2])
-Coef        = Vector(NBvar)
-
-for val in eachindex(values)
-   Coef[val]=parse(values[val])
-end
-@variable(  m,  0 <= x[1:NBvar] <= 1,Int)
-@objective( m , Max, sum( Coef[j] * x[j] for j=1:NBvar ) )
-
-#@variable(  m, x[1:NBvar], Bin )
-#@objective( m, Max, dot( Coef, x ) )
-LeftMembers_Constraints    = spzeros(NBcons,NBvar)
-RightMembers_Constraints   = Vector(NBcons)
-
-deleteat!(lines,1)
-deleteat!(lines,1)
-for line in lines
-   values = split(line)
-   if counter%2 == 1
-      #RightMembers_Constraints[counter2]=parse(values[1])
-      RightMembers_Constraints[counter2]=1
-   else
-      for val in values
-         LeftMembers_Constraints[counter2,parse(val)]=1
-         #parcours chaque valeurs (chaque valeur est separer par un espace)
-         #ecrire dfct get value qui renvoie un tableau
-         #lire dabord la pemiere ligne
-         #puis lire le reste du fichier grace au infos obtenu dans la premiere ligne
+#GETTING DATA FRON FILE
+FileList = readdir("./Data")
+dir = pwd()
+x_cpuTime = Vector(length(FileList))
+y_problemSize = Vector(length(FileList))
+z_problemConstraints = Vector(length(FileList))
+for i in eachindex(FileList)
+   #MODEL CONSTRUCTION
+   #--------------------
+   #m           = Model(solver=GLPKSolverLP())
+   m           = Model(solver=GLPKSolverMIP())
+   #READING DATA FROM FILE
+   #     GETTING NB OF VARIABLES AND CONSTRAINTS
+   #     AND VALUES ASSOCIATED TO THEM
+   filename    = string(dir,"/Data/",FileList[i])
+   workingfile = open(filename);
+   lines       = readlines(workingfile)
+   counter     = 1
+   counter2    = 1
+   VarAndCons  = split(lines[1])
+   NBcons      = parse(VarAndCons[1])
+   NBvar       = parse(VarAndCons[2])
+   if NBcons < 1000 && NBvar < 1000
+      values      = split(lines[2])
+      Coef        = Vector(NBvar)
+      tic()
+      for val in eachindex(values)
+         Coef[val]=parse(values[val])
       end
-      counter2+=1
+      @variable(  m,  0 <= x[1:NBvar] <= 1,Int)
+      @objective( m , Max, sum( Coef[j] * x[j] for j=1:NBvar ) )
+
+      #@variable(  m, x[1:NBvar], Bin )
+      #@objective( m, Max, dot( Coef, x ) )
+      LeftMembers_Constraints    = spzeros(NBcons,NBvar)
+      RightMembers_Constraints   = Vector(NBcons)
+NBvar
+      deleteat!(lines,1)
+      deleteat!(lines,1)
+      for line in lines
+         values = split(line)
+         if counter%2 == 1
+            #RightMembers_Constraints[counter2]=parse(values[1])
+            RightMembers_Constraints[counter2]=1
+         else
+            for val in values
+               LeftMembers_Constraints[counter2,parse(val)]=1
+               #parcours chaque valeurs (chaque valeur est separer par un espace)
+               #ecrire dfct get value qui renvoie un tableau
+               #lire dabord la pemiere ligne
+               #puis lire le reste du fichier grace au infos obtenu dans la premiere ligne
+            end
+            counter2+=1
+         end
+         counter+=1
+      end
+      @constraint( m , cte[i=1:NBcons], sum(LeftMembers_Constraints[i,j] * x[j] for j=1:NBvar) <= RightMembers_Constraints[i] )
+      #@constraint(m, dot(LeftMembers_Constraints, x) <= RightMembers_Constraints)
+      #println("The optimization problem to be solved is:")
+      #print(m) # Shows the model constructed in a human-readable form
+
+      #SOLVE IT AND DISPLAY THE RESULTS
+      #--------------------------------
+      status = solve(m) # solves the model
+      x_cpuTime[i]=toc()
+      y_problemSize[i]=NBvar
+      z_problemConstraints[i]=NBcons
+      println("Objective value: ", getobjectivevalue(m)) # getObjectiveValue(model_name) gives the optimum objective value
    end
-   counter+=1
+   close(workingfile)
 end
-@constraint( m , cte[i=1:NBcons], sum(LeftMembers_Constraints[i,j] * x[j] for j=1:NBvar) <= RightMembers_Constraints[i] )
-#@constraint(m, dot(LeftMembers_Constraints, x) <= RightMembers_Constraints)
-println("The optimization problem to be solved is:")
-print(m) # Shows the model constructed in a human-readable form
-
-#SOLVE IT AND DISPLAY THE RESULTS
-#--------------------------------
-status = solve(m) # solves the model
-
-println("Objective value: ", getobjectivevalue(m)) # getObjectiveValue(model_name) gives the optimum objective value
-for i in 1:NBvar
-   println("x",i," = ", getvalue(x[i]))
+function ReadFile(FileName::string)
+   workingfile = open(FileName);
+   lines       = readlines(workingfile)
+   counter     = 1
+   counter2    = 1
+   VarAndCons  = split(lines[1])
+   NBcons      = parse(VarAndCons[1])
+   NBvar       = parse(VarAndCons[2])
+   values      = split(lines[2])
+   Coef        = Vector(NBvar)
+   for val in eachindex(values)
+      Coef[val]=parse(values[val])
+   end
+   LeftMembers_Constraints    = spzeros(NBcons,NBvar)
+   RightMembers_Constraints   = Vector(NBcons)
+   deleteat!(lines,1)
+   deleteat!(lines,1)
+   for line in lines
+      values = split(line)
+      if counter%2 == 1
+         #RightMembers_Constraints[counter2]=parse(values[1])
+         RightMembers_Constraints[counter2]=1
+      else
+         for val in values
+            LeftMembers_Constraints[counter2,parse(val)]=1
+            #parcours chaque valeurs (chaque valeur est separer par un espace)
+            #ecrire dfct get value qui renvoie un tableau
+            #lire dabord la pemiere ligne
+            #puis lire le reste du fichier grace au infos obtenu dans la premiere ligne
+         end
+         counter2+=1
+      end
+      counter+=1
+   end
+   close(workingfile)
+   return Problem(NBvar,NBcons,Coef,LeftMembers_Constraints,RightMembers_Constraints)
 end
--- myHeuristics.jl
+++ myHeuristics.jl
@@ -4,3 +4,53 @@
 #cette heuristique de depart sera develloper ici
 #Mise en place d'une heuristique de recherche locale de type plus profonde descente
 #Celle ci sera fondee sur deux voisins
+module DM1_metaheuristics
+export CurrentSolution,FindingAdmissingBaseSolution1,DeepLocalSearch,IsConsistent,
+#Order variable by decreasing cost
+#Align the constraints with the variable order
+#Compute the ratio between the cost and the total number of occurence of the variable in all the constraints
+type CurrentSolution
+   NBconstraints::Int
+   NBvariables::Int
+   CurrentVarIndex::Int
+   CurrentObjectiveValue::Int
+   LeftMemberValues::Vector(NBconstraints)
+   Coefficients::Vector(NBvariables)
+   Constraints::Array(NBconstraints,NBvariables)
+end
+function FindingAdmissingBaseSolution1(CS::CurrentSolution)
+   Ratio = Array(2,CS.NBvariables)
+   for i in 1:1:CS.NBvariables
+      nb =  0
+      for j in 1:1:CS.NBconstraints
+            if CS.Constraints[i][j] == 1
+               nb += 1
+            end
+      end
+      Ratio[1][i]= i
+      Ratio[2][i]= CS.Coefficients[i]/nb
+   end
+   sortcols(Ratio)
+   println(Ratio)
+end
+#Fonction recursive permettant l'eploration des solutions voisines admissibles
+function DeepLocalSearch(CS::CurrentSolution,TryingPos::Int)
+
+
+end
+#LastLeftMemberValue sera un vecteur contenant la derniere valeur calculee pour la somme des membres de gauche des contraintes
+#LastModifiedIndex sera un entier ayant pour valeur l'indice de la derniere variable modifie de 0 a 1
+
+function IsConsistent(CS::CurrentSolution)
+   for j in 1:1:CS.NBconstraints
+         if CS.Constraints[j][CS.LastModifiedIndex] == 1
+            if CS.LastLeftMemberValue[j] ==  0
+               CS.LastLeftMemberValue[j] = 1
+            else
+               return false,nothing
+            end
+         end
+   end
+   CS.CurrentObjectiveValue+=CS.Coefficients[CS.LastModifiedIndex]
+   return true,CS
+end
